dc364a686ded66ac70cedb5225eda2116f70ea0b
Seppy <53712490+Seppy-Dev@users.noreply.github.com>
2024-10-14 02:18:17 +0100

    Append -y to line 10

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -7,7 +7,7 @@ sudo dnf update -y
 sudo dnf install unzip -y
 
 #Install RPM Fusion
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
 #Install NVIDIA Drivers
 sudo dnf install akmod-nvidia -y
